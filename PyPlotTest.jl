3d8047a769d9e371cb4fe346a10e4c79db356d70
maikholzhey <maik.holzhey@gmail.com>
2019-04-25 13:59:31 +0200

    How to plot a Field on mesh

--- PyPlotTest.jl
+++ PyPlotTest.jl
@@ -1,15 +1,23 @@
+
+#===============
+ Pkg.add("PyPlot")
+ Pkg.add("Inbteract")
+    in Julia REPL
+===============#
 using Plots
 pyplot()
-plot(Plots.fakedata(50, 5), w=3)
 
-#x = 1:0.5:20
-#y = 1:0.5:10
-#f(x, y) = begin
-        #(3x + y ^ 2) * abs(sin(x) + cos(y))
-    #end
-#X = repeat(reshape(x, 1, :), length(y), 1)
-#Y = repeat(y, 1, length(x))
+# simple plot
+plot(Plots.fakedata(50, 5), w=3)
+# field on x,y mesh
+x = 1:0.5:20
+y = 1:0.5:10
+f(x, y) = begin
+(3x + y ^ 2) * abs(sin(x) + cos(y))
+end
+X = repeat(reshape(x, 1, :), length(y), 1)
+Y = repeat(y, 1, length(x))
 #Z = map(f, X, Y)
-#p1 = contour(x, y, f, fill=true)
+p1 = contour(x, y, f, fill=true)
 #p2 = contour(x, y, Z)
-#plot(p1, p2)
+plot(p1)#, p2)
